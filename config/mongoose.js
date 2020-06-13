82af40bc6ea2c918f479a83f2ddec950a814d265
anurag pandey <garunanurag75@gmail.com>
2020-06-14 00:13:23 +0530

    completed till gulp

---
+++ config/mongoose.js
@@ -0,0 +1,11 @@
+const mongoose=require('mongoose');
+const env=require('./environment');
+mongoose.connect(process.env.MONGODB_URI ||`mongodb://localhost/${env.db}`);
+
+const db=mongoose.connection;
+db.on('error',console.error.bind(console,'error in connecting to Mongodb'));
+
+db.once('open',function(){
+    console.log('Connected to Database:: Mongodb');
+});
+module.exports=db;
